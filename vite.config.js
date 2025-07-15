8a8d487121bfd5439831482e7f1ea1b10b7ca631
wuselwiesel <wuselwiesel@outlook.com>
2025-07-15 10:50:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
     host: '0.0.0.0',
-    hmr: true, // Change this line to false disable auto-refreshing.
+    hmr: true, // das kannst du auch einfach weglassen, ist nur für lokale Dev
+  },
+  build: {
+    outDir: 'dist',         // <--- wichtig! genau dieser Ordner wird von Express ausgeliefert
+    emptyOutDir: true,      // vorher alles in dist löschen
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html') // falls Vite den Einstieg nicht erkennt
+    }
   }
-})
+});
